cb31041aadcbcc4b02e21829674a7521a73504c0
alexfess <alexanderfess@gmail.com>
2020-12-07 13:42:56 +0300

    Добавляем зависимость в gradle

--- src/main/java/org/alexfess/gradle_java11_test.java
+++ src/main/java/org/alexfess/gradle_java11_test.java
@@ -1,7 +1,18 @@
 package org.alexfess;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class gradle_java11_test {
+    public String getMessage() {
+        return "Hello World";
+    }
+
+    public gradle_java11_test() {
+        System.out.println(getMessage());
+        System.out.println(StringUtils.upperCase(getMessage()));
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        new gradle_java11_test();
     }
 }
